d33f5be8e9f0b9101c726771f9a3909f8188f8d7
Edwin de Jonge <edwindjonge@gmail.com>
2026-03-04 13:35:11 +0100

    updating

--- src/muxpack/multiplex.py
+++ src/muxpack/multiplex.py
@@ -10,15 +10,21 @@ class Multiplex:
     _edges: ibis.Table
     _vertices: ibis.Table
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, edges: ibis.Table, vertices: ibis.Table = None) -> None:
+        self._edges = edges
+
+        if (vertices is None) and (edges is not None):
+            self._vertices = edges.distinct("src").union(
+                edges.distinct("dst")
+            )
+        self._vertices = vertices
     
-    def years(self, t: ibis.Table) -> list[int]:
-        years = t.distinct("year").execute()
+    def years(self) -> list[int]:
+        years = self._edges.distinct("year").execute()
         return years
     
-    def layers(self, t: ibis.Table) -> list[str]:
-        layers = t.distinct("layer").execute()
+    def layers(self) -> list[str]:
+        layers = self._edges.distinct("layer").execute()
         return layers
     
     def edges(self) -> ibis.Table:
