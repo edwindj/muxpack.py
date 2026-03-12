e372e180946fd9c314774b1b7d5d6b5c4078a0be
Edwin de Jonge <edwindjonge@gmail.com>
2026-03-12 10:21:52 +0100

    wip

--- src/muxpack/__init__.py
+++ src/muxpack/__init__.py
@@ -1,35 +1,5 @@
-import ibis
-from .multiplex import *
+from .check import check_edges, check_vertices
+from .io import load_network
+from .multiplex import Multiplex
 
-def load_network(dir: str) -> Multiplex:
-    """
-    Load a multiplex network from a directory containing Parquet files.
-
-    Parameters
-    ----------
-    dir : str
-        The directory containing the Parquet files. The expected structure is:
-        dir/
-            year1/
-                edges/
-                    layer1/
-                        *.parquet
-                    layer2/
-                        *.parquet
-            year2/
-                edges/
-                    layer1/
-                        *.parquet
-                    layer2/
-    
-    years and layers are also in the parquet files, but the directory structure is expected to follow this pattern.
-    
-    Returns
-    -------
-    Multiplex
-        The loaded multiplex network.
-    """
-    print("Loading data...")
-    edges = ibis.read_parquet(f"{dir}/*/edges/**/*.parquet", table_name="edges")
-    m = Multiplex(edges=edges)
-    return m
+__all__ = ["check_edges", "check_vertices", "load_network", "Multiplex"]
