b32d2ba056364a0ad64874ee73c4a92da8534242
Edwin de Jonge <edwindjonge@gmail.com>
2026-03-16 16:22:33 +0100

    wip

---
+++ src/muxpack/to_csr_matrix.py
@@ -0,0 +1,69 @@
+from ibis import row_number, Table
+import ibis
+from scipy.sparse import csr_matrix
+from muxpack.multiplex import Multiplex
+
+def to_row_col_idx(edges: Table, vertices: Table) -> Table:
+    """
+    Turn the edge list into a row col index table, based on the 
+    vertices table that is given.
+
+    Args:
+        edges : needs `src` and `dst` field
+        vertices : needs an `id` field, edges will be filtered on id.
+ 
+    Returns:
+        lazy edges table that only contains edges with id in vertices. 
+        the row and column refer to the row number of the vertices Table.
+        The result can be used by `to_csr_matrix`
+    """
+    v = vertices.select("id").mutate(idx = row_number())
+    row = v.select(src = "id", row = "idx")
+    col = v.select(dst = "id", col = "idx")
+
+    idx_edges = (
+        edges
+        .inner_join(row, "src")
+        .inner_join(col, "dst")
+        .mutate(data = True)
+        .select("data", "row", "col")
+    )
+    print(idx_edges.execute())
+    return idx_edges
+
+def idx_to_csr_matrix(idx: Table, vertices: Table) -> csr_matrix:
+    # TODO maybe to_parquet()?
+    coo = idx.execute()
+    # print(coo)
+
+    n = vertices.count().execute()
+    # print(f"n: {n}, \ncoo:{coo}")
+    M = csr_matrix((coo["data"], (coo["row"], coo["col"])), shape=(n,n))
+    return M
+
+def to_csr_matrix(edges: Table, vertices: Table) -> csr_matrix:
+    edges_row_col = to_row_col_idx(edges, vertices=vertices)
+    M = idx_to_csr_matrix(edges_row_col, vertices=vertices)
+    return 
+
+
+if __name__ == "__main__":
+    import pandas as pd
+    edges = pd.DataFrame({
+        "src" : [100,100],
+        "dst" : [300,200]
+    })
+    vertices = pd.DataFrame({
+        "id": [100,200,300]
+    })
+
+    E = ibis.memtable(edges)
+    V = ibis.memtable(vertices)
+    
+    V1 = V.filter(V.id < 250)
+    idx = to_row_col_idx(E, V1)
+    M1 = idx_to_csr_matrix(idx, V1)
+    print(f"M1 = {M1}")
+
+    M = to_csr_matrix(E, V)
+    print(M)
