575f257a5dff08989a563045bc86b7a13a7faa92
Edwin de Jonge <edwindjonge@gmail.com>
2026-03-31 09:47:30 +0200

    updating stuff

--- docs/api.rst
+++ docs/api.rst
@@ -1,7 +1,63 @@
-API
-===
+API Reference
+=============
 
-.. autosummary::
-   :toctree: generated
+.. contents:: Modules
+   :local:
+   :depth: 1
 
-   muxpack
+
+MultiplexSeries
+---------------
+
+.. autoclass:: muxpack.MultiplexSeries
+   :members:
+   :special-members: __init__
+
+
+Multiplex
+---------
+
+.. autoclass:: muxpack.Multiplex
+   :members:
+   :special-members: __init__
+
+
+BiPartite
+---------
+
+.. autoclass:: muxpack.bipartite.BiPartite
+   :members:
+   :special-members: __init__
+
+
+Validation
+----------
+
+.. autofunction:: muxpack.check_edges
+.. autofunction:: muxpack.check_vertices
+.. autofunction:: muxpack.check.check_column_type
+
+
+Input / Output
+--------------
+
+.. autofunction:: muxpack.load_network
+.. autofunction:: muxpack.save_network
+.. autofunction:: muxpack.io.save_multiplex
+.. autofunction:: muxpack.io.save_multiplexseries
+.. autofunction:: muxpack.io.save_bipartite
+.. autofunction:: muxpack.io.read_bipartite
+
+
+Sparse matrices
+---------------
+
+.. autofunction:: muxpack.to_csr_matrix
+.. autofunction:: muxpack.to_csr_matrix.to_period_csr_matrix
+
+
+NetworkX
+--------
+
+.. autofunction:: muxpack.networkx.to_MultiDiGraph
+
--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,19 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+from pathlib import Path
+
+# Make the muxpack package importable when building docs.
+sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "muxpack"
 copyright = "2026, Edwin de Jonge, Jan van der Laan"
 author = "Edwin de Jonge, Jan van der Laan"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -16,8 +23,10 @@ author = "Edwin de Jonge, Jan van der Laan"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
+    "sphinx_rtd_theme",
 ]
 
 templates_path = ["_templates"]
@@ -25,15 +34,41 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 language = "en"
 
+# -- Autodoc configuration ---------------------------------------------------
+
+autosummary_generate = True
+
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "show-inheritance": True,
+    "special-members": "__init__",
+}
+
+# Heavy C-extension dependencies may not always be available during builds;
+# mock them so autodoc can still import and document the Python modules.
+autodoc_mock_imports = ["scipy"]
+
+# -- Napoleon (docstring) configuration --------------------------------------
+# The project uses Google-style Args/Returns sections.
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_use_param = False
+napoleon_use_rtype = False
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-from pathlib import Path
 
-html_theme = "alabaster"
-if Path(__file__).with_name("_static").exists():
-    html_static_path = ["_static"]
-else:
-    html_static_path = []
+html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    "navigation_depth": 3,
+    "titles_only": False,
+}
+
+html_static_path = []
 
 # -- Options for todo extension ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
