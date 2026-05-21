3520ca71c6da38a67d57ccedb991c1f8cf23f5e8
Edwin de Jonge <edwindjonge@gmail.com>
2026-05-21 11:47:11 +0200

    updating

--- tests/test_to_csr_matrix.py
+++ tests/test_to_csr_matrix.py
@@ -8,7 +8,7 @@ from scipy.sparse import csr_matrix
 
 @pytest.fixture()
 def edges_vertices() -> Generator[Tuple[ibis.Table, ibis.Table]]:
-    E = ibis.memtable({"src": [100, 200, 300], "dst": [200, 100, 200]})
+    E = ibis.memtable({"src": [100, 100, 200], "dst": [200, 300, 300]})
 
     V = ibis.memtable({"id": [100, 200, 300]})
     yield E, V
@@ -18,5 +18,12 @@ def test_to_csr_matrix(edges_vertices):
     edges, vertices = edges_vertices
     m = muxpack.to_csr_matrix(edges, vertices)
 
+    assert isinstance(m, csr_matrix)
+    print(m.toarray())
+
+def test_to_weighted_csr_matrix(edges_vertices):
+    edges, vertices = edges_vertices
+    edges = edges.mutate(weight=[1.0, 2.0, 3.0])
+    m = muxpack.to_csr_matrix(edges, vertices, use_weight="weight")
     assert isinstance(m, csr_matrix)
     print(m.toarray())
